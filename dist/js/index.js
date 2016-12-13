248f9aec29f23fac7f56c1d9fea51f7d98e718b7
Ben Beauregard <bot190@gmail.com>
2016-12-13 11:07:29 -0500

    Adjusted colors, and only graph the last 30 datapoints.

--- dist/js/index.js
+++ dist/js/index.js
@@ -4,12 +4,12 @@ bandwidthDatasets= [
         label: "Up",
         fill: true,
         lineTension: 0.1,
-        backgroundColor: "rgba(76,180,219,0.4)",
-        borderColor: "rgba(76,180,219,1)",
+        backgroundColor: "rgba(63, 149, 255,0.3)",
+        borderColor: "rgba(63, 149, 255,1)",
         borderJoinStyle: 'miter',
-        pointBorderColor: "rgba(75,192,192,1)",
+        pointBorderColor: "rgba(63, 149, 255,1)",
         pointBackgroundColor: "#fff",
-        pointHoverBackgroundColor: "rgba(76,180,219,1)",
+        pointHoverBackgroundColor: "rgba(63, 149, 255,1)",
         pointHoverBorderColor: "rgba(220,220,220,1)",
         pointHoverBorderWidth: 2,
         pointRadius: 1,
@@ -20,12 +20,12 @@ bandwidthDatasets= [
         label: "Down",
         fill: true,
         lineTension: 0.1,
-        backgroundColor: "rgba(15, 198, 88,0.4)",
-        borderColor: "rgba(15, 198, 88,1)",
+        backgroundColor: "rgba(44, 198, 27,0.3)",
+        borderColor: "rgba(44, 198, 27,1)",
         borderJoinStyle: 'miter',
         pointBorderColor: "rgba(75,192,192,1)",
         pointBackgroundColor: "#fff",
-        pointHoverBackgroundColor: "rgba(15, 198, 88,1)",
+        pointHoverBackgroundColor: "rgba(44, 198, 27,1)",
         pointHoverBorderColor: "rgba(220,220,220,1)",
         pointHoverBorderWidth: 2,
         pointRadius: 1,
@@ -70,6 +70,12 @@ function updateCharts (chart,newdata) {
 	chart.data.labels.push(newdata.labels);
 	chart.data.datasets[0].data = chart.data.datasets[0].data.concat(newdata.up);
 	chart.data.datasets[1].data = chart.data.datasets[1].data.concat(newdata.down);
+	// Only show the last 20 data points
+	if (chart.data.labels.length >= 30) {
+		chart.data.labels.splice(0,1);
+		chart.data.datasets[0].data.splice(0,1);
+		chart.data.datasets[1].data.splice(0,1);
+	}
 	chart.update();
 }
 
